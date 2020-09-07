8046eb82b55caa1c2d055ba805fcb83c072af8f3
minoura-a <minoura_a@dcinc.co.jp>
2020-09-07 16:58:18 +0900

    はてなブログの人気記事を表示するwebアプリ作成

---
+++ HtenaBlogDisplay/RssInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace HtenaBlogDisplay
+{
+	public class RssInfo
+	{
+		[JsonProperty("titleList")]
+		public List<string> TitleList =new List<string>();
+
+		[JsonProperty("urlList")]
+		public List<string> UrlList =new List<string>();
+	}
+}
